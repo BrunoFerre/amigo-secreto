31584097c1cd88f7f7cecc4bc7c12a84b48807bb
Bruno Ferreira <fbrunomarcos@gmail.com>
2025-01-25 17:16:11 -0300

    Proyecto Terminado

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//capturar elementos del DOM
+let inputNombre = document.getElementById("amigo");
+let arrayAmigos = [];
+const $litaAmigos = document.getElementById("listaAmigos");
+const $resultado = document.getElementById("resultado");
+//Funcion para agregar amigos al array
+function agregarAmigo() {
+  if (inputNombre.value === "") {
+    alert("Debes ingresar un nombre");
+    return;
+  }
+  arrayAmigos.push(inputNombre.value);
+  console.log(arrayAmigos);
+  inputNombre.value = "";
+  inputNombre.focus();
+  $litaAmigos.innerHTML = `<li>${arrayAmigos}</li>`;
+  return arrayAmigos;
+}
+
+//funcion para sortear amigo secreto
+function sortearAmigoSecreto() {
+  let amigoSecreto = arrayAmigos[Math.floor(Math.random() * arrayAmigos.length)];
+  console.log(amigoSecreto);
+  $resultado.innerHTML = `<li>${amigoSecreto}</li>`;
+  return amigoSecreto;
+}
